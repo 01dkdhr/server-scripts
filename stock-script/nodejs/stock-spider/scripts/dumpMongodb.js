229c4304691edf0455c55a59d31cdaa107700cdc
kdeng <kdeng@hillinsight.com>
2018-09-11 10:38:07 +0800

    add

---
+++ stock-script/nodejs/stock-spider/scripts/dumpMongodb.js
@@ -0,0 +1,12 @@
+const exec = require('child_process').exec;
+const localConfig = require('../local-config.json');
+
+if (localConfig && localConfig['mongo-db-path']) {
+    var cmd = `mongod --dbpath=${localConfig['mongo-db-path']}`;
+
+    exec(cmd, function(error, stdout, stderr) {
+        error && console.log('err:', error);
+    });
+} else {
+    console.log('err: not find mongodb config')
+}
