29ee90ec1dae3a4416360d4b64b4ea807292e192
kdeng <kdeng@hillinsight.com>
2018-09-11 10:45:46 +0800

    提交脚本

--- stock-script/nodejs/stock-spider/scripts/dumpMongodb.js
+++ stock-script/nodejs/stock-spider/scripts/dumpMongodb.js
@@ -1,12 +1,18 @@
+const path = require('path');
 const exec = require('child_process').exec;
 const localConfig = require('../local-config.json');
 
-if (localConfig && localConfig['mongo-db-path']) {
-    var cmd = `mongod --dbpath=${localConfig['mongo-db-path']}`;
+if (localConfig && localConfig['is-server'] && localConfig['mongo-config'] && localConfig['mongo-config']['db-name']) {
+    const dbName = localConfig['mongo-config']['db-name'];
+    const user = localConfig['mongo-config']['user'];
+    const password = localConfig['mongo-config']['password'];
+    const output = path.join(__dirname, '../dump-mongodb');
+
+    var cmd = `mongodump -h 127.0.0.1 -d ${dbName} -u ${user} -p ${password} -o ${output}`;
 
     exec(cmd, function(error, stdout, stderr) {
         error && console.log('err:', error);
     });
 } else {
-    console.log('err: not find mongodb config')
+    console.log('err: localConfig is err');
 }
