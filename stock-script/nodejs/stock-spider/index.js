df74bb51ede7a2f0addcf576d8f24433e8a2e3dd
kdeng <kdeng@hillinsight.com>
2018-09-11 12:37:45 +0800

    提交代码

--- stock-script/nodejs/stock-spider/index.js
+++ stock-script/nodejs/stock-spider/index.js
@@ -5,6 +5,11 @@ const TimeUtil = require('./utils/TimeUtil.js');
 function main() {
     console.log('last trade date:', TimeUtil.formatDate(TimeUtil.lastTradeDay(), true));
 
+    if ((new Date()).getHours() < 15) {
+        console.log('未到15点,无法同步当天数据');
+        return;
+    }
+
     StockCrawlerService.fetchAllStocks()
     .then((stocks) => {
         if (stocks && stocks.length) {
