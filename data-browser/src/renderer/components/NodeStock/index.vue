216d5e3440ad0648ff6f063b2065204406ba1b86
kdeng <369069851@hillinsight.com>
2018-09-29 18:31:33 +0800

    提交代码

--- data-browser/src/renderer/components/NodeStock/index.vue
+++ data-browser/src/renderer/components/NodeStock/index.vue
@@ -1,28 +1,125 @@
 <template>
 <div id="node-stock">
     <div class="tool-bar">
+        <!-- 数据库加载状态 -->
         <span class="db-state" 
             :class="[stateData.dbInited ? 'success' : 'fail']">
             {{stateData.dbInited ? '数据加载成功' : '数据未加载'}}
         </span>
+        <!-- 时间选择 -->
+        <div class="date-area">
+            <input type="radio" id="group-date" value="group-date" v-model="dateMode">
+            <span>起始时间:</span>
+            <input :disabled="dateMode !== 'group-date'" type="date" name="start-date" v-model="dateGroup.startDate"/>
+            <span>截止时间:</span>
+            <input :disabled="dateMode !== 'group-date'" type="date" name="end-date" v-model="dateGroup.endDate"/>
+        </div>
+        <div class="date-area">
+            <input type="radio" id="single-date" value="single-date" v-model="dateMode">
+            <span>单一时间:</span>
+            <input :disabled="dateMode !== 'single-date'" type="date" name="single-date" v-model="dateSingle.date"/>
+        </div>
+        <!-- 市场选择 -->
+        <div class="market-select">
+            <span>市场:</span>
+            <select v-model="marketMode">
+                <option v-for="(mode, index) in marketList" :value="mode" :key="index">{{mode}}</option>
+            </select>
+        </div>
     </div>
     <div class="top-place-holder"></div>
+    <!-- 股票表格 -->
+    <table border="1" class="stock-table">
+    <tr>
+        <th v-for="head in tableHeader" :key="head">{{getShowWord(head)}}</th>
+    </tr>
+    <tr v-for="stock in showStocks" :key="stock.fullStockCode">
+        <td v-for="head in tableHeader" :key="head">{{stock[head] || '-'}}</td>
+    </tr>
+    </table>
 </div>
 </template>
 
 <script>
 import { ipcRenderer } from 'electron';
 import service from './js/service.js';
+import TimeUtil from '@/utils/TimeUtil.js';
+import _ from 'lodash';
+
+const defaultTableHeader = ['stockName', 'fullStockCode', 'curPrice', 'averagePrice', 'buyVolume', 'yesterdayClosePrice', 'openPrice', 'marketValue', 'high', 'low', 'pb', 'pe', 'amountRatio', 'turnoverRate', 'tradeVolume', 'tradeValue', 'sellVolume', 'entrustRatio', 'circulationValue', 'superFlowIn', 'superFlowOut', 'bigFlowIn', 'bigFlowOut', 'middleFlowIn', 'middleFlowOut', 'littleFlowIn', 'littleFlowOut'];
 export default {
     name: 'node-stock',
     data() {
         return {
-            
+            dateGroup: {
+                startDate: 0,
+                endDate: 0
+            },
+            dateSingle: {
+                date: 0
+            },
+            dateMode: 'single-date',
+            marketList: ['all', 'sh', 'sz'],
+            marketMode: 'all',
+            tableHeader: defaultTableHeader,
+            sortKeys: [] // 排序字段
         }
     },
     computed: {
         stateData() {
             return this.$store.state.NodeStock;
+        },
+        showStocks() {
+            const list = [];
+            this.stateData.stocks.slice(0, 10).forEach((stock) => {
+                if (this.marketMode !== 'all' && stock.market !== this.marketMode) {
+                    return;
+                }
+
+                const item = {
+                    stockName: stock.stockName,
+                    fullStockCode: stock.fullStockCode    
+                };
+
+                if (this.dateMode == 'single-date') {
+                    const dailyStock = _.find(this.stateData.dailyStocks, { 
+                        fullStockCode: stock.fullStockCode,
+                        dateTime: TimeUtil.transStringToDate(this.dateSingle.date)
+                    }); 
+                    
+                    if (dailyStock) {
+                        Object.assign(item, dailyStock);
+                        list.push(item);
+                    }
+                } else if (this.dateMode == 'group-date') {
+                    const start = TimeUtil.transStringToDate(this.dateGroup.startDate);
+                    const end = TimeUtil.transStringToDate(this.dateGroup.endDate);
+                    const dailyStocks = _.filter(this.stateData.dailyStocks, (e) => {
+                        return ((e.fullStockCode == stock.fullStockCode) 
+                            && (e.dateTime >= start) && (e.dateTime <= end));
+                    }); 
+
+                    if (dailyStocks && dailyStocks.length) {
+                        Object.Keys(dailyStocks).forEach((key) => {
+                            let count = 0;
+                            let totalVaule = 0;
+                            dailyStocks.forEach((e) => {
+                                if (e[key]) {
+                                    totalVaule += e[key];
+                                    count ++;    
+                                }
+                            });   
+                            
+                            item[key] = count ? (totalVaule / count) : 0;
+                        });
+                        
+                        list.push(item);
+                    }
+                }
+
+            });
+
+            return list;
         }
     },
     created() {
@@ -40,11 +137,75 @@ export default {
                 this.$store.commit('SET_STOCKS', stocks);
                 this.$store.commit('SET_DAILY_STOCKS', dailyStocks);
                 this.$store.commit('SET_DATE_ARRAY', dateArray);
-                this.$store.commit('SET_DB_INITED', true);        
+                this.$store.commit('SET_DB_INITED', true);   
+                
+                this.dateGroup.startDate = TimeUtil.transDateToString(this.stateData.dateArray[this.stateData.dateArray.length - 1]);
+                this.dateGroup.endDate = TimeUtil.transDateToString(this.stateData.dateArray[0]);
+                this.dateSingle.date = this.dateGroup.endDate;
             })
             .catch((err) => {
                 console.log('asyncLoadData err:', err)
             });
+        },
+        getShowWord(key) {
+            switch(key) {
+            case 'stockName':
+                return '名称';
+            case 'fullStockCode':
+                return '代码';
+            case 'amountRatio':
+                return '数量比率';
+            case 'averagePrice':
+                return '平均价格';
+            case 'bigFlowIn':
+                return '大流入';
+            case 'bigFlowOut':
+                return '大流出';
+            case 'buyVolume':
+                return '购买量';
+            case 'circulationValue':
+                return '流通价值';
+            case 'curPrice':
+                return '当前价格';
+            case 'entrustRatio':
+                return '委托比率';
+            case 'high':
+                return '最高价';
+            case 'littleFlowIn':
+                return '小流入';
+            case 'littleFlowOut':
+                return '小流出';
+            case 'low':
+                return '最低价';
+            case 'marketValue':
+                return '市场价值';
+            case 'middleFlowIn':
+                return '中流入';
+            case 'middleFlowOut':
+                return '中流出';
+            case 'openPrice':
+                return '今日开价';
+            case 'pb':
+                return '平均市净率';
+            case 'pe':
+                return '市盈率';
+            case 'sellVolume':
+                return '销售量';
+            case 'superFlowIn':
+                return '超级流入';
+            case 'superFlowOut':
+                return '超级流出';
+            case 'tradeValue':
+                return '交易值';
+            case 'tradeVolume':
+                return '交易量';
+            case 'turnoverRate':
+                return '换手率';
+            case 'yesterdayClosePrice':
+                return '昨日收价';
+            default:
+                return '';
+            }
         }
     },
     beforeRouteEnter (to, from, next) {
@@ -60,6 +221,11 @@ export default {
             }
         ]);
         next();
+    },
+    watch: {
+        // marketMode: (newValue) => {
+        //     console.log(newValue)
+        // }
     }
 }
 </script>
@@ -89,6 +255,10 @@ export default {
             }
         }
 
+        .date-area, .market-select {
+            margin-left: 20px;
+        }
+
         .init-db-btn {
             margin-left: 10px;
             width: 80px;
@@ -98,6 +268,14 @@ export default {
     .top-place-holder {
         position: static;
         height: 30px;
+    }
+
+    .stock-table {
+        th, td
+        {
+            white-space: nowrap;
+            padding: 3px;
+        }
     }
 }
 </style>
---
+++ data-browser/src/renderer/utils/TimeUtil.js
@@ -0,0 +1,19 @@
+const TimeUtil = {
+    transDateToString(date) {
+        const tmpDate = new Date(date);
+        let month = tmpDate.getMonth() + 1;
+        month = (month.toString().length == 1) ? ("0" + month) : month;
+        let day = tmpDate.getDate();
+        day = (day.toString().length == 1) ? ("0" + day) : day;
+
+        return `${tmpDate.getFullYear()}-${month}-${day}`;
+    },
+    transStringToDate(str) {
+        if (!str) {
+            return 0;
+        }
+        return new Date(str + ' 00:00:00').getTime();
+    }
+};
+
+export default TimeUtil;
